bde83088a9dd45caf9c5888db00134516378e2bf
sadiqua10-khan <sadiqua10@gmail.com>
2026-01-01 11:44:45 +0530

    initial commit

---
+++ arithmetic.py
@@ -0,0 +1,10 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+addition=a+b
+subtraction = a-b
+multiplication=a*b
+division=a/b
+print("addition is :",addition)
+print ("subtraction is :",subtraction)
+print("multiplication is :",multiplication)
+print("division is :",round(division))
---
+++ personalgreet.py
@@ -0,0 +1,4 @@
+first_name = input("enter your first name :")
+second_name = input("enter your last name :")
+
+message = print("hello, " + first_name + " "  + second_name + "!", "welcome to the python world")
